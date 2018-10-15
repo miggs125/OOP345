bb504b560d81b73987cc82c1505df0fda931c86d
miguel_roncancio <mroncancio@myseneca.ca>
2018-10-14 22:53:33 -0400

    practice for midterm

---
+++ Grades/practice.cpp
@@ -0,0 +1,69 @@
+#include <iostream> 
+
+enum class Letter // enumeration example
+    { A, B, C };
+
+template <typename T, int G = 0> // template syntax
+class List {
+    public:
+    T m_array*;
+    size_t arr_size;
+    T dummy;
+
+    // custom constructor
+    List() : arr_size{G}, m_array{nullptr} { }
+
+    //copy constructor
+    List& List(const List& other){ *this = other; }
+
+    // move constructor
+    List& List(List&& other){ *this = std::move(other); }
+
+    // copy assignment operator
+    List& operator=(const List& other){
+
+    }
+
+  
+
+
+
+
+
+    // move assignment operator 
+
+
+
+
+
+    // overloaded [] operator
+    const T& operator[](size_t index) const {
+        if(index >= 0 && index < count)
+            return m_array[index];
+	    else
+	        return dummy;
+    };
+
+
+
+
+    // overloaded += operator
+    void operator+=(const T& newLetterGrade) // overloaded operator that adds an element to the array if space is available
+    {
+        
+    
+    }
+
+
+
+
+
+
+};
+
+
+int main()
+{
+
+    return 0;
+}
